ab4614e7519fa441beb50f02b3c09e3372886828
sapoverde <agflores2011@gmail.com>
2025-04-12 18:49:24 +0100

    Mid-project commit

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
+from constants import *
 
 def main():
-    print("Starting Asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    #print("Starting Asteroids!")
+    #print(f"Screen width: {constants.SCREEN_WIDTH}")
+    #print(f"Screen height: {constants.SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
